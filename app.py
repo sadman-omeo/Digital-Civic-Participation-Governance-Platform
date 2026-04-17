4eb8d83103747931fce4021d6d4193af3610e146
SADMAN AHMED OMEO <sadmanahmed167@gmail.com>
2026-04-17 22:14:40 +0600

    Result visualization feature 200 OK

--- app.py
+++ app.py
@@ -91,6 +91,14 @@ app.register_blueprint(chat_bp)
 from routes.notification import notification_bp, generate_notifications 
 app.register_blueprint(notification_bp)  
 
+#result visualization routes
+from routes.results import result_bp  
+app.register_blueprint(result_bp)  
+
+
+
+
+
 # Create Database Tables
 with app.app_context():
     db.create_all()
